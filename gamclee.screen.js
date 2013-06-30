f5d980839be6c0295f1fbc8599a788b383bb1a26
Kevin S <kevin@chilicode.ca>
2013-06-30 13:10:35 -0700

    - Fixed pause/resume on focus/blur - Added debounce method to misc

--- gamclee.screen.js
+++ gamclee.screen.js
@@ -20,18 +20,21 @@ define(['gamclee.misc', 'gamclee.timer', 'gamclee.events'], function(Misc, Timer
 		this.width = canvas.width;
 		this.height = canvas.height;
 		
+		this.isHidden = false;
 		this.isFullScreen = false;
+		this.isPaused = false;
+		
 		this.frameLimit = 0.1;
-		this.pause = false;
 		this.lastFrameTime = -1;
 		this.lastFrameLength = 0;
 		this.lastFPSCheck = 0;
 		this.lastFPS = 0;
 		this.fpsCounter = 0;
 		
-		// Bind native dom events
-		Events.addDomEventListener('blur', this.pause);
-		Events.addDomEventListener('focus', this.resume);
+		// Set-up pause/resume on window focus
+		var screenInstance = this;
+		Events.addDomEventListener('focus', window, Misc.debounce(function() { screenInstance.visible(); }, 100, true));
+		Events.addDomEventListener('blur', window, Misc.debounce(function() { screenInstance.hidden(); }, 100, true));
 		
 		// Set-up the "draw" loop
 		var objContext = this, drawHandler = function(){
@@ -48,17 +51,27 @@ define(['gamclee.misc', 'gamclee.timer', 'gamclee.events'], function(Misc, Timer
 		return this.canvas;
 	};
 	
+	Screen.prototype.hidden = function() {
+		this.pause();
+		this.isHidden = true;
+	};
+	
+	Screen.prototype.visible = function() {
+		this.resume();
+		this.isHidden = false;
+	}
+	
 	Screen.prototype.pause = function() {
-		this.pause = true;
+		this.isPaused = true;
 	};
 	
 	Screen.prototype.resume = function() {
-		this.pause = false;
+		this.isPaused = false;
 	};
 	
 	Screen.prototype.draw = function() {
 		// If paused, do nothing!
-        if(this.pause) {
+        if(this.isPaused) {
             return;
         }
 			
